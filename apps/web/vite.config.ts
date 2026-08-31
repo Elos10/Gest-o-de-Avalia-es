9ad72f7bc0eb4975b22912cc055e700f1dc073ab
Eder Leonardo <140769660+Elos10@users.noreply.github.com>
2026-08-31 10:21:04 -0300

    segunda3108_1021

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,1 +1,1 @@
-import {defineConfig} from 'vite';import react from '@vitejs/plugin-react';export default defineConfig({envDir:'../..',plugins:[react()],server:{port:5173}});
+import {defineConfig} from 'vite';import react from '@vitejs/plugin-react';export default defineConfig({base:process.env.VITE_BASE_PATH??'/',envDir:'../..',plugins:[react()],server:{port:5173}});
